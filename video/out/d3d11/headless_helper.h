1a92f48304c380388853f9b43729c83c140905a5
dixeran <dixeran@outlook.com>
2019-12-29 14:40:41 +0800

    bridge between libmpv and vo_d3d11
    Add methods so when vo_d3d11 init, it can pass out the created D3D11Device and Swapchian; when in render loop, it can retrieve new render size from libmpv.

---
+++ video/out/d3d11/headless_helper.h
@@ -0,0 +1,5 @@
+// methods defined in libmpv_dxgi.c
+bool libmpv_dxgi_swc_out(void* pIDXGISwapChain);
+bool libmpv_dxgi_dev_out(void* pID3D11Device);
+int libmpv_dxgi_get_width(void);
+int libmpv_dxgi_get_height(void);
